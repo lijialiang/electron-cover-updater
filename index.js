a7299ad1ccb7ac3b64f141da0ad8100144cb9124
lijialiang <lijialiang@yy.com>
2019-05-14 09:26:04 +0800

    chore(release): 1.0.0

--- index.js
+++ index.js
@@ -1,95 +1,91 @@
-'use strict';
+const fs = require('fs')
+const path = require('path')
 
-const fs = require('fs');
-const path = require('path');
-
-const updater = { };
+const updater = {}
 
 updater.option = {
-    root: '',
-    versionURL: '',
-    versionField: '',
-    zipURL: '',
-    isDiffSystem: false,
-    nowVersion: '',
-    packageJsonFile: '',
-    temp: path.resolve( __dirname, './temp' ),
-};
-
-updater.config = ( option ) => {
-    updater.option = Object.assign( updater.option, option );
-
-    const { temp } = updater.option;
-
-    !fs.existsSync( temp ) ? fs.mkdirSync( temp ) : void 0;
+  root: '',
+  versionURL: '',
+  versionField: '',
+  zipURL: '',
+  isDiffSystem: false,
+  nowVersion: '',
+  packageJsonFile: '',
+  temp: path.resolve(__dirname, './temp')
 }
 
-updater.check = function ( ) {
-    const { option: { nowVersion } } = updater;
-    return new Promise( ( resolve, reject ) => {
-        require('./lib/get_version').bind( updater )( )
-            .then( version => {
-                const result = require('compare-versions')( version, nowVersion );
+updater.config = option => {
+  updater.option = Object.assign(updater.option, option)
 
-                resolve({
-                    version,
-                    isNeedUpdate: result > 0 ? true : false,
-                } );
-            } )
-            .catch( err => console.error( err ) )
-    } );
+  const { temp } = updater.option
+
+  !fs.existsSync(temp) ? fs.mkdirSync(temp) : void 0
 }
 
-let updatingCallback = void 0;
+updater.check = function () {
+  const { option: { nowVersion } } = updater
+  return new Promise((resolve, reject) => {
+    require('./lib/get_version').bind(updater)()
+      .then(version => {
+        const result = require('compare-versions')(version, nowVersion)
 
-const updatingTotalStep = 6;
-let updatingStep = 0;
-
-updater.updating = function ( arg ) {
-    let progress = 0;
-
-    if ( typeof arg === 'string' ) {
-        progress = parseInt( ( ++updatingStep / updatingTotalStep ) * 100 );
-    }
-
-    typeof arg === 'function' ? updatingCallback = arg : (
-        updatingCallback ? updatingCallback( arg, progress ) : void 0
-    );
-};
-
-updater.update_version = void 0;
-
-updater.update = function ( version ) {
-    const { updating } = updater;
-
-    const set_update_version = ( { version: _version } ) => {
-        return new Promise( ( resolve, reject ) => {
-            !version ? version = _version : void 0;
-            updating( `updating version ${ version }` );
-            updater.update_version = version;
-            resolve( { version } );
-        } );
-    }
-
-    return new Promise( ( resolve, reject ) => {
-        updater.check( )
-            .then( set_update_version )
-            .then( require('./lib/download_zip').bind( updater ) )
-            .then( require('./lib/unzip_cover_files').bind( updater ) )
-            .then( require('./lib/clean').bind( updater ) )
-            .then( require('./lib/save_version').bind( updater ) )
-            .then( ( ) => {
-                updatingStep = 0;
-
-                resolve( );
-            } )
-            .catch( e => {
-                updatingStep = 0;
-
-                reject( e );
-            } )
-    } );
+        resolve({
+          version,
+          isNeedUpdate: result > 0
+        })
+      })
+      .catch(err => console.error(err))
+  })
 }
 
+let updatingCallback = void 0
 
-module.exports = updater;
+const updatingTotalStep = 6
+let updatingStep = 0
+
+updater.updating = function (arg) {
+  let progress = 0
+
+  if (typeof arg === 'string') {
+    progress = parseInt((++updatingStep / updatingTotalStep) * 100)
+  }
+
+  typeof arg === 'function' ? updatingCallback = arg : (
+    updatingCallback ? updatingCallback(arg, progress) : void 0
+  )
+}
+
+updater.update_version = void 0
+
+updater.update = function (version) {
+  const { updating } = updater
+
+  /* eslint-disable */
+  const set_update_version = ({ version: _version }) => {
+    return new Promise((resolve, reject) => {
+      !version ? version = _version : void 0
+      updating(`updating version ${version}`)
+      updater.update_version = version
+      resolve({ version })
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    updater.check()
+      .then(set_update_version)
+      .then(require('./lib/download_zip').bind(updater))
+      .then(require('./lib/unzip_cover_files').bind(updater))
+      .then(require('./lib/clean').bind(updater))
+      .then(require('./lib/save_version').bind(updater))
+      .then(() => {
+        updatingStep = 0
+        resolve()
+      })
+      .catch(err => {
+        updatingStep = 0
+        reject(err)
+      })
+  })
+}
+
+module.exports = updater
--- test/index.js
+++ test/index.js
@@ -1,27 +1,24 @@
-'use strict';
+const path = require('path')
 
-const path = require('path');
-
-const updater = require('../index');
+const updater = require('../index')
 
 updater.config({
-    root: path.resolve( __dirname, './temp/' ),
-    versionURL: 'https://legox.org/assets/drog/version.1.1.json',
-    versionField: 'version',
-    zipURL: 'https://legox.org/assets/drog',
-    isDiffSystem: false,
-    packageJsonFile: path.resolve( __dirname, './test.json' ),
-    nowVersion: '1.0.0',
+  root: __dirname,
+  versionURL: 'http://fimoapp.com/hub/index.json',
+  versionField: 'version',
+  zipURL: 'http://fimoapp.com/hub/',
+  packageJsonFile: path.resolve(__dirname, './test.json'),
+  nowVersion: '1.0.0'
 })
 
-updater.check().then(( { version, isNeedUpdate } ) => {
-    if ( isNeedUpdate ) {
-        updater.update().then(() => {
-            console.log( '>>>>>>>>>>>>>>>> update success' );
-        });
-    }
-});
+updater.check().then(({ version, isNeedUpdate }) => {
+  if (isNeedUpdate) {
+    updater.update().then(() => {
+      console.log('>>>>>>>>>>>>>>>> update success')
+    })
+  }
+})
 
-updater.updating(( msg, progress ) => {
-    console.log( msg, progress );
+updater.updating((msg, progress) => {
+  console.log(msg, progress)
 })
